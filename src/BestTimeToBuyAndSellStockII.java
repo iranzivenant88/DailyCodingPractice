ad79c8fd7d4837dbf700d3a1e92dd4f8f8e38478
iranzivenant88 <46546731+iranzivenant88@users.noreply.github.com>
2023-01-21 14:48:41 -0700

    BestTimeToBuyAndSellStockII

--- src/BestTimeToBuyAndSellStockII.java
+++ src/BestTimeToBuyAndSellStockII.java
@@ -18,15 +18,15 @@ Total profit is 4.*/
 
         public static int maxProfit(int[] prices)
         {
-            int profit = 0;
+            int TotalProfit = 0;
             for(int i = 0;i<prices.length-1;i++)
             {
                 if(prices[i]<prices[i+1])
                 {
-                    profit+=prices[i+1]-prices[i];
+                    TotalProfit+=prices[i+1]-prices[i];
                 }
             }
-            return profit;
+            return TotalProfit;
         }
 
     public static void main(String[] args) {
