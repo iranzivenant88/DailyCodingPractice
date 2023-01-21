709e19228beee0f6b96cf05fa9a245bc41658743
iranzivenant88 <46546731+iranzivenant88@users.noreply.github.com>
2023-01-21 13:41:50 -0700

    added questions and comments

--- src/GroupAnagram1.java
+++ src/GroupAnagram1.java
@@ -1,11 +1,24 @@
 import java.util.*;
 
 public class GroupAnagram1 {
-
+    /*
+  * Given an array of strings strs, group the anagrams together. You can return the answer in any order.
+An Anagram is a word or phrase formed by rearranging the letters of a different word or phrase,
+* typically using all the original letters exactly once.
+Example 1:
+Input: strs = ["eat","tea","tan","ate","nat","bat"]
+Output: [["bat"],["nat","tan"],["ate","eat","tea"]]
+Example 2:
+Input: strs = [""]
+Output: [[""]]
+Example 3:
+Input: strs = ["a"]
+Output: [["a"]]
+ */
         public static List<List<String>>groupAnagrams(String[] str)
         {
             //Key = Sorted String
-            //Value list of original String
+            //Value =list of original String
             Map<String,List<String>> map = new HashMap<>();
             List<List<String>>results = new ArrayList<>();
 
