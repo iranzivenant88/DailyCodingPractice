a64c5be08218545cd3c048fce5a1bcb997b29f41
iranzivenant88 <46546731+iranzivenant88@users.noreply.github.com>
2023-01-22 11:42:13 -0700

    Rotate an array clockwise

---
+++ src/RotateArray.java
@@ -0,0 +1,41 @@
+public class RotateArray {
+    /*
+ Given an integer array nums, rotate the array to the right by k steps, where k is non-negative.
+Example 1:
+Input: nums = [1,2,3,4,5,6,7], k = 3
+Output: [5,6,7,1,2,3,4]
+Explanation:
+rotate 1 steps to the right: [7,1,2,3,4,5,6]
+rotate 2 steps to the right: [6,7,1,2,3,4,5]
+rotate 3 steps to the right: [5,6,7,1,2,3,4]
+Example 2:
+Input: nums = [-1,-100,3,99], k = 2
+Output: [3,99,-1,-100]
+Explanation:
+rotate 1 steps to the right: [99,-1,-100,3]
+rotate 2 steps to the right: [3,99,-1,-100]
+ * */
+    static void reverse(int[] arr,int startingPoint,int endingPoint){
+        while(endingPoint>startingPoint){
+            int temporaryVariable = arr[startingPoint];
+            arr[startingPoint]=arr[endingPoint];
+            arr[endingPoint] =temporaryVariable;
+            startingPoint++;
+            endingPoint--;
+        }
+    }
+    public static void main(String[] args) {
+        int [] array ={2,4,56,21,43,77};
+        int k =2; // k is the number of times to rotate our array
+        int len = array.length; // this is the length of the array
+
+        reverse(array,0,k-1);
+        reverse(array,k,len-1);
+        reverse(array,0,len-1);
+
+        for (int i: array){
+            System.out.print(i +" ");
+        }
+        }
+    }
+
