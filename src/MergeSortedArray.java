0e2f0955d7f2d37720213eb60cbb13303cd8073d
iranzivenant88 <46546731+iranzivenant88@users.noreply.github.com>
2023-03-04 10:24:03 -0700

    Program to Merge Sorted Array

---
+++ src/MergeSortedArray.java
@@ -0,0 +1,42 @@
+public class MergeSortedArray
+ {
+    public static void merge(int[] nums1, int m, int[] nums2, int n) {
+        int p = m+n-1;
+        int p1 = m-1;
+        int p2 = n-1;
+
+        while(p1>=0 && p2>=0){
+            if(nums1[p1]>nums2[p2]){
+                nums1[p] = nums1[p1];
+                p1--;
+                p--;
+
+            }else{
+                nums1[p] = nums2[p2];
+                p2--;
+                p--;
+
+            }
+        }
+
+        while(p2>=0){
+            nums1[p]=nums2[p2];
+            p2--;
+            p--;
+        }
+
+    }
+
+     public static void main(String[] args) {
+         int[] nums1 = {1,2,3,0,0,0};
+         int m = 3;
+         int[]nums2 = {2,5,6};
+         int n = 3;
+         merge(nums1,m,nums2,n);
+         for (int a :nums1) {
+             System.out.print(a+" ");
+
+         }
+
+     }
+}
