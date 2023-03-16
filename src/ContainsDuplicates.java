28cc6eba4846d3333c0803c8cc297a27eaaaa2c0
“iranzivenant123” <iranzivenant88@gmail.com>
2023-03-16 01:53:26 -0600

    containing duplicatesB

--- src/ContainsDuplicates.java
+++ src/ContainsDuplicates.java
@@ -1,17 +1,21 @@
 import java.util.Arrays;
 
 public class ContainsDuplicates {
-    public boolean containsDuplicate(int[] nums) {
-        if (nums.length <= 1 || nums == null) {
+    public boolean containsDuplicate(int[] nums)
+    {
+        if (nums.length <= 1 || nums == null)
+        {
             return false;
         }
         Arrays.sort(nums);
         int i = 0;
         int j = 1;
-        while (j < nums.length) {
+        while (j < nums.length)
+        {
             if (nums[i] == nums[j]) {
                 return true;
-            } else {
+            } else
+            {
                 i++;
                 j++;
             }
