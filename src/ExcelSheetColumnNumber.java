ec90d33af8dbed7de31bcb2e4d7f700db444bd24
“iranzivenant123” <iranzivenant88@gmail.com>
2023-03-17 01:04:35 -0600

    ExcelSheetColumnNumber-2

--- src/ExcelSheetColumnNumber.java
+++ src/ExcelSheetColumnNumber.java
@@ -5,7 +5,6 @@ public class ExcelSheetColumnNumber {
         for (int i = 0; i < columnTitle.length(); i++) {
             result *= 26;
             result += (columnTitle.charAt(i) - 'A' + 1);
-
         }
         return result;
     }
