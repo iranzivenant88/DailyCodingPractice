e50f5de7d6347d99f3342a0461792657b4bdba51
“iranzivenant123” <iranzivenant88@gmail.com>
2023-03-17 22:20:17 -0600

    new changes

--- src/ExcelSheetColumnNumber.java
+++ src/ExcelSheetColumnNumber.java
@@ -8,5 +8,4 @@ public class ExcelSheetColumnNumber {
         }
         return result;
     }
-
 }
--- src/PlusOne.java
+++ src/PlusOne.java
@@ -33,9 +33,9 @@ Thus, the result should be [1,0].*/
         for(int i = digits.length-1;i>=0;i--){
             if(digits[i]<9){
                 digits[i]++;
-                return digits;
+                return digits;//[1,3,0]
             }
-            digits[i]=0;
+            digits[i]=0; //[1,2,0]
         }
         int []result = new int[digits.length+1];
         result[0]=1;
@@ -44,7 +44,7 @@ Thus, the result should be [1,0].*/
 
 }
     public static void main(String[] args) {
-        int [] input = {1, 2, 3};
+        int [] input = {1, 2,9};
       int []rest  = plusOne(input);
         System.out.println(Arrays.toString(rest));
 
