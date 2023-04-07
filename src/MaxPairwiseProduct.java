43758b39120fb42501dd9abf1f388511dbf7fa4a
iranzivenant88 <88venant@gmail.com>
2023-04-07 11:44:07 -0600

    changes

--- src/MaxPairwiseProduct.java
+++ src/MaxPairwiseProduct.java
@@ -2,58 +2,31 @@ import java.util.*;
 import java.io.*;
 
     public class MaxPairwiseProduct {
-        static int getMaxPairwiseProduct(int[] numbers) {
-            int max_product = 0;
-            int n = numbers.length;
+            static long getMaxPairwiseProduct(int[] numbers) {
+                int n = numbers.length;
+                long max1 = -1, max2 = -1;
 
-            for (int first = 0; first < n; ++first) {
-                for (int second = first + 1; second < n; ++second) {
-                    max_product = Math.max(max_product,
-                            numbers[first] * numbers[second]);
-                }
-            }
-
-            return max_product;
-        }
-
-        public static void main(String[] args) {
-            FastScanner scanner = new FastScanner(System.in);
-            int n = scanner.nextInt();
-            int[] numbers = new int[n];
-            for (int i = 0; i < n; i++) {
-                numbers[i] = scanner.nextInt();
-            }
-            System.out.println(getMaxPairwiseProduct(numbers));
-        }
-
-        static class FastScanner {
-            BufferedReader br;
-            StringTokenizer st;
-
-            FastScanner(InputStream stream) {
-                try {
-                    br = new BufferedReader(new
-                            InputStreamReader(stream));
-                } catch (Exception e) {
-                    e.printStackTrace();
-                }
-            }
-
-            String next() {
-                while (st == null || !st.hasMoreTokens()) {
-                    try {
-                        st = new StringTokenizer(br.readLine());
-                    } catch (IOException e) {
-                        e.printStackTrace();
+                for (int i = 0; i < n; ++i) {
+                    if (numbers[i] > max1) {
+                        max2 = max1;
+                        max1 = numbers[i];
+                    } else if (numbers[i] > max2) {
+                        max2 = numbers[i];
                     }
                 }
-                return st.nextToken();
+
+                return max1 * max2;
             }
 
-            int nextInt() {
-                return Integer.parseInt(next());
+            public static void main(String[] args) {
+                Scanner scanner = new Scanner(System.in);
+                int n = scanner.nextInt();
+                int[] numbers = new int[n];
+                for (int i = 0; i < n; i++) {
+                    numbers[i] = scanner.nextInt();
+                }
+                System.out.println(getMaxPairwiseProduct(numbers));
             }
-        }
 
     }
 
