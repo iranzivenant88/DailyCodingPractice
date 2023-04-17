de3596b943bd9eb23784f9b4172e9c6519685d28
Venant N Bigwi <46546731+iranzivenant88@users.noreply.github.com>
2023-04-16 23:15:05 -0600

    Time: 30 ms (8.38%), Space: 43.3 MB (5.66%) - LeetHub

---
+++ 0009-palindrome-number/0009-palindrome-number.java
@@ -0,0 +1,19 @@
+class Solution {
+    public boolean isPalindrome(int x) {
+        String str = String.valueOf(x);
+        int len = str.length();
+        //int i = 0;
+        String reverse = "";
+        
+        for(int i =len-1 ;i>=0;i--){
+            reverse+=str.charAt(i);
+            if(str.equals(reverse)){
+                return true;
+            }
+         
+            
+        }
+        return false;
+        
+    }
+}
