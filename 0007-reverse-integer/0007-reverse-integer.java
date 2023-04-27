1f6db8c98dad6c77fcf3b2442f5c83440f035f2f
Venant N Bigwi <46546731+iranzivenant88@users.noreply.github.com>
2023-04-26 22:03:11 -0600

    Time: 1 ms (100.00%), Space: 40.2 MB (27.86%) - LeetHub

---
+++ 0007-reverse-integer/0007-reverse-integer.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int reverse(int x) {
+        boolean isNegative = false;
+       int reverse = 0;
+        if(x<0){
+            isNegative = true;
+            x = x *(-1);
+        } 
+        while(x != 0){
+        int pop = x %10 ; 
+        
+            x = x/10;
+        
+        if (reverse > Integer.MAX_VALUE/10 || (reverse == Integer.MAX_VALUE / 10 && pop > 7)) return 0;
+        if (reverse < Integer.MIN_VALUE/10 || (reverse == Integer.MIN_VALUE / 10 && pop < -8)) return 0;
+            reverse = reverse * 10 + pop; 
+        }
+        if(isNegative){
+            reverse = (-1)*reverse;
+        }        
+        return reverse;
+        
+    }
+}
