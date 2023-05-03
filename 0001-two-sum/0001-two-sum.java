7e2c4b781bae92e79e5298b1b40937aa18d15fed
Venant N Bigwi <46546731+iranzivenant88@users.noreply.github.com>
2023-05-03 07:10:40 -0600

    Time: 1 ms (99.35%), Space: 43 MB (30.07%) - LeetHub

--- 0001-two-sum/0001-two-sum.java
+++ 0001-two-sum/0001-two-sum.java
@@ -1,22 +1,20 @@
 class Solution {
     public int[] twoSum(int[] nums, int target) {
-      Map<Integer,Integer>map = new HashMap<>();
-        for (int i=0;i<nums.length;i++)
-        {
-            int compliment =target-nums[i];
-            if (map.containsKey(compliment))
-            {
-               int arr[]= {map.get(compliment),i};
-               return arr;
-            } else
-            {
+        Map<Integer,Integer>map = new HashMap<>();
+        for(int i =0;i<nums.length;i++){
+            int compliment = target-nums[i];
+            if(map.containsKey(compliment)){
+                int a [] = {i,map.get(compliment)};
+            //return new int [] {map.get(compliment),i};
+             return a;
+    
+            }else{
                 map.put(nums[i],i);
             }
-
+            
+        
         }
-
-        throw new IllegalArgumentException();
-   
+        return null;
+        
     }
 }
-
