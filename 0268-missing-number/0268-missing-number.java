17a304e4a52c4ad5ec23d64aad6bb52be76d1573
Venant N Bigwi <46546731+iranzivenant88@users.noreply.github.com>
2023-05-26 22:21:12 -0600

    Time: 0 ms (100.00%), Space: 44.7 MB (5.95%) - LeetHub

--- 0268-missing-number/0268-missing-number.java
+++ 0268-missing-number/0268-missing-number.java
@@ -1,21 +1,13 @@
 class Solution {
     public int missingNumber(int[] nums) {
-         Arrays.sort(nums);
-
-        int left = 0;
-        int right = nums.length - 1;
-
-        while (left <= right) {
-            int mid = left + (right - left) / 2;
-
-            if (nums[mid] == mid) {
-                left = mid + 1;
-            } else {
-                right = mid - 1;
-            }
+         int missing = nums.length;
+        
+        for (int i = 0; i < nums.length; i++) {
+            missing ^= nums[i];
+            missing ^= i;
         }
-
-        return left;
+        
+        return missing;
     }
  
 }
