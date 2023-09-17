22c4ae7245186adcfdebfb14d0b7c8cca3b936d7
Venant N Bigwi <46546731+iranzivenant88@users.noreply.github.com>
2023-09-17 09:54:26 -0600

    Time: 2 ms (64.08%), Space: 45.2 MB (28.49%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.java
+++ 0283-move-zeroes/0283-move-zeroes.java
@@ -1,22 +1,19 @@
 class Solution {
     public void moveZeroes(int[] nums) {
-       int i =0;
-        int j = 1 ;
-        while (j<nums.length){
-            if(nums[i] ==0 && nums[j]!=0){
-                int temp = nums[i];
-                nums[i] = nums[j];
-                nums[j] = temp;
-                j++;
-                i++;
-            }else if(nums[i]==0 &&nums[j]==0){
-                j++;
-                       
-                 }else {
-                i++;
-                j++;
+        int counterOfNonZeroNumbers = 0;
+        for(int i = 0;i<nums.length;i++){
+            if(nums[i]!=0){
+                //Input: nums = [0,1,0,3,12]           
+                nums[counterOfNonZeroNumbers]=nums[i];  
+                counterOfNonZeroNumbers++;
             }
+             
+            }
+            while (counterOfNonZeroNumbers < nums.length){
+              
+                nums[counterOfNonZeroNumbers]=0;
+                 counterOfNonZeroNumbers ++;
         }
+        
     }
 }
-
