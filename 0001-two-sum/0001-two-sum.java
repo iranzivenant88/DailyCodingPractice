ab44bca92ae919e22480ef94bd615d339f654aa3
Venant N Bigwi <46546731+iranzivenant88@users.noreply.github.com>
2023-10-25 19:16:10 -0600

    Time: 1 ms (99.39%), Space: 43.6 MB (49.90%) - LeetHub

--- 0001-two-sum/0001-two-sum.java
+++ 0001-two-sum/0001-two-sum.java
@@ -1,16 +1,15 @@
 class Solution {
     public int[] twoSum(int[] nums, int target) {
-    Map<Integer, Integer> map = new HashMap<>();
-   
-    for(int i = 0;i<nums.length;i++){
-        int compliment = target-nums[i];
-        if(map.containsKey(compliment)){
-            int []result = {map.get(compliment),i};
-           return result;  
+    Map<Integer,Integer>map = new HashMap<>();
+        for(int i = 0 ; i< nums.length;i++){
+            int compliment = target - nums[i];
+           if(map.containsKey(compliment)){
+               int [] result = {map.get(compliment),i};
+               return result;
+            }else{
+                map.put(nums[i],i);
+            }
         }
-        map.put(nums[i],i);
-    }
-   
-    throw new ArithmeticException("There is no correct answer ");
+        return null;
     }
 }
