5da42fa09d44d6d40e7ec210d9015f32325baa2b
Venant N Bigwi <46546731+iranzivenant88@users.noreply.github.com>
2023-12-29 19:24:25 -0800

    Time: 7 ms (32.99%), Space: 43.1 MB (69.90%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.java
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -1,26 +1,20 @@
-
 class Solution {
     public boolean isAnagram(String s, String t) {
-        // Check if the lengths of the two strings are different
-        if (s.length() != t.length()) {
+        if(s.length() != t.length()) {
             return false;
         }
+        
+        int [] arr = new int [26]; 
+        for(int i = 0; i < s.length(); i++) {
+             arr[s.charAt(i)- 'a']++; 
+            arr[t.charAt(i)- 'a'] --;
+        } 
+        for(int c : arr) {
+            if(c!=0) {
+                return false;
+            }
 
-        // Create two HashMaps to store character frequencies for each string
-        Map<Character, Integer> map1 = new HashMap<>();
-        Map<Character, Integer> map2 = new HashMap<>();
-
-        // Populate map1 with character frequencies from string s
-        for (int i = 0; i < s.length(); i++) {
-            map1.put(s.charAt(i), map1.getOrDefault(s.charAt(i), 0) + 1);
         }
-
-        // Populate map2 with character frequencies from string t
-        for (int j = 0; j < t.length(); j++) {
-            map2.put(t.charAt(j), map2.getOrDefault(t.charAt(j), 0) + 1);
-        }
-
-        // Compare the character frequencies in map1 and map2
-        return map1.equals(map2);
+        return true;
     }
 }
