fa9b8c266dd5add3f687d0000d6680c4b6e6f788
Venant N Bigwi <46546731+iranzivenant88@users.noreply.github.com>
2024-01-08 20:06:58 -0700

    Time: 0 ms (100.00%), Space: 42.8 MB (7.07%) - LeetHub

--- 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.java
+++ 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.java
@@ -10,29 +10,55 @@
  */
 class Solution {
     public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
-        ListNode head = new ListNode();
-        ListNode mainHead = head;
-        while(list1 != null && list2 != null){
-            if(list1.val >=list2.val){
-                head.next = list2;
-                head = head.next;
-                list2 = list2.next;
-            }else {
-                head.next = list1;
-                head = head.next;
-                list1 = list1.next;
+        if(list1 == null && list2 == null){
+            return null;
+        }
+        if(list1 == null){
+            return list2;
+        }
+        if(list2 == null){
+            return list1;
+        }
+        ListNode node1 = list1;
+        ListNode node2 = list2;
+        ListNode result = new ListNode(0);
+        ListNode resultCopy = result;
+        
+        while(node1 != null && node2 != null){
+            // if(node1.val == node2.val){
+            //     result.next = node1;
+            //     result = result.next;
+            //     result.next = node2;
+            //     result = result.next;
+            //     node1 = node1.next;
+            //     node2 = node2.next;
+                
+           // }else 
+            
+            if( node1.val <= node2.val){
+                result.next = node1;
+                result = result.next;
+                node1 = node1.next;
+            }else{
+                result.next = node2;
+                result = result.next;
+                node2 = node2.next;
+                
             }
-        } 
-        while ( list1!=null){
-            head.next = list1;
-            list1 = list1.next;
-            head = head.next;
+            
         }
-        while(list2!=null){
-            head.next = list2;
-            list2 = list2.next;
-            head = head.next;
+        while(node1 != null){
+            result.next = node1;
+            node1 = node1.next;
+            result = result.next;
         }
-        return mainHead.next;
+        while(node2 != null){
+                 result.next = node2;
+            node2 = node2.next;
+            result = result.next;
+        }
+        
+        return resultCopy.next;
+        
     }
 }
