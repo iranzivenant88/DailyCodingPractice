030d3ec8fe839437593ce8763e7fe22d5ac279d7
Venant N Bigwi <46546731+iranzivenant88@users.noreply.github.com>
2024-01-08 20:15:51 -0700

    Time: 0 ms (100.00%), Space: 43 MB (7.07%) - LeetHub

--- 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.java
+++ 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.java
@@ -10,53 +10,35 @@
  */
 class Solution {
     public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
-        if(list1 == null && list2 == null){
-            return null;
-        }
-        if(list1 == null){
-            return list2;
-        }
-        if(list2 == null){
-            return list1;
-        }
         ListNode node1 = list1;
         ListNode node2 = list2;
         ListNode result = new ListNode(0);
         ListNode resultCopy = result;
         
-        while(node1 != null && node2 != null){
-            // if(node1.val == node2.val){
-            //     result.next = node1;
-            //     result = result.next;
-            //     result.next = node2;
-            //     result = result.next;
-            //     node1 = node1.next;
-            //     node2 = node2.next;
-                
-           // }else 
-            
+        while(node1 != null && node2 != null){         
             if( node1.val <= node2.val){
                 result.next = node1;
-                result = result.next;
+               // result = result.next;
                 node1 = node1.next;
             }else{
                 result.next = node2;
-                result = result.next;
+               // result = result.next;
                 node2 = node2.next;
                 
             }
-            
+            result = result.next;   
         }
-        while(node1 != null){
+        if(node1 != null){
             result.next = node1;
-            node1 = node1.next;
-            result = result.next;
+            // node1 = node1.next;
+            // result = result.next;
         }
-        while(node2 != null){
-                 result.next = node2;
-            node2 = node2.next;
-            result = result.next;
+        if(node2 != null){
+            result.next = node2;
+            // node2 = node2.next;
+            // result = result.next;
         }
+        // We are return resultCopy.next because the result started with dummy value
         
         return resultCopy.next;
         
