b39a7e3c6b80a901a9dec2e8aae8cf6febd41f06
Venant N Bigwi <46546731+iranzivenant88@users.noreply.github.com>
2024-02-18 23:55:39 -0700

    Time: 0 ms (100.00%), Space: 41.8 MB (69.60%) - LeetHub

--- 0027-remove-element/0027-remove-element.java
+++ 0027-remove-element/0027-remove-element.java
@@ -3,24 +3,23 @@ class Solution {
 
     // nums = [3,2,2,3], val = 3
     // 2,2,3
+        //[2,2,3]
+        
     // [2,2,-,-]
-    
-        int i = 0; 
-        int j = 0; 
-        int k = 0; 
-        while( j < nums.length) {
-            if(nums[j] == val) {
-                j++; // nums[i] = nums[j]
-            } else {
-                nums[i] = nums[j]; 
-                i++; 
-                j++; 
-                k++; 
-            
-            }
+        int i = 0;
+        int j = 0;
+        int k = 0;
+    while(j<nums.length){
+        if(nums[j] == val){
+            j++;
+        }else{
+            nums[i] = nums[j];
+            i++;
+            j++;
+            k++;
         }
-
-        return k; 
+    }
+        return k ; 
         
     }
 }
